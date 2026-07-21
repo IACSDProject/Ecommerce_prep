77ce736b0827b4824664747eb8dcaa58fee00064
umesh mane <umeshmane575@gmail.com>
2026-07-21 21:10:43 +0530

    SignOut enpoint added

--- sb-ecom/src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
+++ sb-ecom/src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
@@ -51,6 +51,14 @@ public class JwtUtils {
         return cookie;
     }
 
+
+    public ResponseCookie getCleanJwtCookie(){
+        ResponseCookie cookie = ResponseCookie.from(jwtCookie, null)
+                .path("/api")
+                .build();
+        return cookie;
+    }
+
     public String generateTokenFromUsername(String username) {
         return Jwts.builder()
                 .subject(username)
