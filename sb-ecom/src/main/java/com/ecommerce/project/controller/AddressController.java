e58f36fcd1e797b14b7903554a38cb393de4864a
umesh mane <umeshmane575@gmail.com>
2026-07-25 20:29:42 +0530

    Geeting address endpoint

--- sb-ecom/src/main/java/com/ecommerce/project/controller/AddressController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/AddressController.java
@@ -8,10 +8,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -29,4 +28,13 @@ public class AddressController {
         return new ResponseEntity<>(savedAddressDTO, HttpStatus.CREATED);
 
     }
+
+    @GetMapping("/addresses")
+    public ResponseEntity<List<AddressDTO>> getAddress() {
+        List<AddressDTO> addressList = addressService.getAddresses();
+        return new ResponseEntity<>(addressList, HttpStatus.OK);
+
+    }
+
+
 }
