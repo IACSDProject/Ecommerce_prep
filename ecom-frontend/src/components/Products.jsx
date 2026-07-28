708f53085ba30245c8b2d1490d019be0b258697b
umesh mane <umeshmane575@gmail.com>
2026-07-28 19:47:03 +0530

    ProductCard Component

--- ecom-frontend/src/components/Products.jsx
+++ ecom-frontend/src/components/Products.jsx
@@ -10,7 +10,7 @@ const Products = () => {
             productName: "Iphone Xs max",
             image: "https://placehold.co/600x400",
             description: "Experience the latest in mobile technology with advanced cameras, powerful processing, and an all-day battery.",
-            quantity: 0,
+            quantity: 10,
             price: 1450.0,
             discount: 10.0,
             specialPrice: 1305.0,
@@ -30,19 +30,19 @@ const Products = () => {
         <div className="lg:px-14 sm:px-8 px-4 py-14 2xl:w-[90%] 2xl:mx-auto">
             {isLoading ? (
                 <p>It is Loading</p>
-            ):errorMessage ? (
-                <div  className="flex justify-center items-center h-[200px]">
+            ) : errorMessage ? (
+                <div className="flex justify-center items-center h-[200px]">
                     <FaExclamationTriangle className="text-slate-800 text-3xl mr-2"/>
                     <span className="text-slate-800 text-lg font-medium">
                         {errorMessage}
                     </span>
                 </div>
-            ): (
+            ) : (
                 <div className="min-h-175">
-                  <div className="pb-6 pt-14 grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-y-6 gap-x-6">
-                      {products && products.map((item , i) => <ProductCard key={i} {...item}/>
-                      )}
-                  </div>
+                    <div className="pb-6 pt-14 grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-y-6 gap-x-6">
+                        {products && products.map((item, i) => <ProductCard key={i} {...item}/>
+                        )}
+                    </div>
 
                 </div>
             )
@@ -51,4 +51,4 @@ const Products = () => {
     )
 }
 
-export  default  Products;
+export default Products;
