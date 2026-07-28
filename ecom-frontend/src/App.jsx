1a95dbdd5c9890c66fc442960755bf5d4d20f137
umesh mane <umeshmane575@gmail.com>
2026-07-28 18:25:57 +0530

    Buiding Product component

--- ecom-frontend/src/App.jsx
+++ ecom-frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import {FaBeer} from "react-icons/fa";
+import Products from "./components/Products";
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold text-red-600 items-center pt-4 pl-5 bg-gray-700">
-      <h3> Lets go for a <FaBeer /> </h3>
-    </h1>
+ <>
+     <Products/>
+ </>
   )
 }
 
