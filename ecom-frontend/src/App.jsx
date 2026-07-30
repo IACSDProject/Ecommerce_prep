78c8122851f770ec08ef68d70f3b5a6d28dfcfde
umesh mane <umeshmane575@gmail.com>
2026-07-30 22:11:02 +0530

    ROutung is setup

--- ecom-frontend/src/App.jsx
+++ ecom-frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from "./components/home/Home.jsx";
 import Products from "./components/products/Products.jsx";
 
 function App() {
 
     return (
-        <>
-            <Products/>
-        </>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/products" element={<Products/>}/>
+            </Routes>
+
+        </Router>
     )
 }
 
---
+++ ecom-frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            Home welcome
+        </div>
+    )
+}
+
+export default Home;
