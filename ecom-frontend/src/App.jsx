017f1b5b3f96a1775ce94b4e94711f90c6466cfe
umesh mane <umeshmane575@gmail.com>
2026-07-30 23:43:23 +0530

    Navbar UI completed

--- ecom-frontend/src/App.jsx
+++ ecom-frontend/src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./components/home/Home.jsx";
 import Products from "./components/products/Products.jsx";
+import Navbar from "./components/shared/Navbar.jsx";
 
 function App() {
 
     return (
         <Router>
+            <Navbar/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/products" element={<Products/>}/>
