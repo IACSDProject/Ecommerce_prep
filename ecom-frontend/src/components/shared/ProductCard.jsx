840d7b50541c1911bf46fe3c213e8ed10f2d1bc8
umesh mane <umeshmane575@gmail.com>
2026-07-31 10:34:44 +0530

    Product Add to Cart functinality

--- ecom-frontend/src/components/shared/ProductCard.jsx
+++ ecom-frontend/src/components/shared/ProductCard.jsx
@@ -2,6 +2,10 @@ import {useState} from "react";
 import ProductViewModal from "./ProductViewModal.jsx";
 import truncateText from "../../utils/truncateText.js";
 import {FaShoppingCart} from "react-icons/fa";
+import {addToCart} from "../../store/actions/index.js";
+import toast from "react-hot-toast";
+import {useDispatch} from "react-redux";
+
 
 const ProductCard = ({
                          productId,
@@ -18,6 +22,7 @@ const ProductCard = ({
     const btnLoader = false;
     const [selectedViewProduct, setSelectedViewProduct] = useState("");
     const isAvailable = quantity && Number(quantity) > 0;
+    const dispatch = useDispatch();
 
     const handleProductView = (product) => {
         if (!about) {
@@ -25,6 +30,10 @@ const ProductCard = ({
             setOpenProductViewModal(true);
         }
 
+    };
+
+    const addToCartHandler = (cartItems) => {
+        dispatch(addToCart(cartItems, 1, toast));
     };
 
 
@@ -94,8 +103,15 @@ const ProductCard = ({
                         )}
                         <button
                             disabled={!isAvailable || btnLoader}
-                            onClick={() => {
-                            }}
+                            onClick={() => addToCartHandler({
+                                image,
+                                productName,
+                                description,
+                                specialPrice,
+                                price,
+                                productId,
+                                quantity,
+                            })}
                             className={`bg-blue-500 ${isAvailable ? "opacity-100 hover:bg-blue-600" : "opacity-70"}
                         text-white py-2 px-3 rounded-lg items-center transition-colors duration-300 w-36 flex justify-center`}>
                             <FaShoppingCart className="mr-2"/>
