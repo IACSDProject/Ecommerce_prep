4f04127b2238590f89b30d1be762cf2bde22f7d9
Kulkarni Varun Uddhav <kvarunuddhav@gmail.com>
2026-08-05 01:40:48 +0530

    Add Stripe payment integration

--- ecom-frontend/src/App.jsx
+++ ecom-frontend/src/App.jsx
@@ -12,6 +12,7 @@ import LogIn from "./components/auth/Login.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
 import Register from "./components/auth/Register.jsx";
 import Checkout from "./components/checkout/Checkout.jsx";
+import PaymentConfirmation from "./components/checkout/PaymentConfirmation.jsx";
 
 function App() {
 
@@ -29,6 +30,7 @@ function App() {
 
                     <Route path='/' element={<PrivateRoute/>}>
                         <Route path='/checkout' element={<Checkout/>}/>
+                        <Route path='/order-confirm' element={<PaymentConfirmation/>}/>
                     </Route>
 
                     <Route path='/' element={<PrivateRoute publicPage/>}>
--- sb-ecom/src/main/java/com/ecommerce/project/controller/OrderController.java
+++ sb-ecom/src/main/java/com/ecommerce/project/controller/OrderController.java
@@ -2,8 +2,12 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.payload.OrderDTO;
 import com.ecommerce.project.payload.OrderRequestDTO;
+import com.ecommerce.project.payload.StripePaymentDto;
 import com.ecommerce.project.service.OrderService;
+import com.ecommerce.project.service.StripeService;
 import com.ecommerce.project.util.AuthUtil;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +21,9 @@ public class OrderController {
 
     @Autowired
     private AuthUtil authUtil;
+
+    @Autowired
+    private StripeService stripeService;
 
     @PostMapping("/order/users/payments/{paymentMethod}")
     public ResponseEntity<OrderDTO> orderProducts(@PathVariable String paymentMethod,
@@ -33,4 +40,13 @@ public class OrderController {
         );
       return  new ResponseEntity<>(order , HttpStatus.CREATED);
     }
+
+    @PostMapping("/order/stripe-client-secret")
+    public ResponseEntity<String> createStripeClientSecret(@RequestBody StripePaymentDto stripePaymentDto) throws StripeException {
+//        System.out.println("StripePaymentDTO Received " + stripePaymentDto);
+        PaymentIntent paymentIntent = stripeService.paymentIntent(stripePaymentDto);
+        return new ResponseEntity<>(paymentIntent.getClientSecret(), HttpStatus.CREATED);
+    }
+
 }
+
