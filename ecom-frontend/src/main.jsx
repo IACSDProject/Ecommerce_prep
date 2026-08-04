d95061a04e693753c53c91dde9c957f0bf7694c9
Kulkarni Varun Uddhav <kvarunuddhav@gmail.com>
2026-08-04 18:15:38 +0530

    Sending cart to backend from frontend

--- ecom-frontend/src/main.jsx
+++ ecom-frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -8,8 +7,6 @@ import store from "./store/reducers/store.js";
 
 createRoot(document.getElementById('root')).render(
     <Provider store={store}>
-        <StrictMode>
-            <App/>
-        </StrictMode>
+        <App/>
     </Provider>,
 )
